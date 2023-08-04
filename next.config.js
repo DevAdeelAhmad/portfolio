6c95fe4e4dd43d9bc3eafbbea141011dbe08b683
DevAdeelAhmad <fa20-bse-031@cuilahore.edu.pk>
2023-08-04 15:41:18 +0500

    complete

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
-  },
   experimental: {
     serverActions: true,
   },
